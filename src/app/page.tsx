75405bd73e97c228eddf94b409f987e538066253
VoidyCd <voidycodes@gmail.com>
2024-07-01 17:01:02 +0300

    footer container added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,24 @@ const oswald = Oswald({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function Home() {
   return (
-    <main className='{oswald.className}'>
-      <div className="flex flex-col items-center justify-center min-h-screen">
+    <main className={`{oswald.className} flex flex-col min-h-screen`}>
+      <div className="flex-grow flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-900">
         <div className="text-center mb-auto mt-8">
           <h1 id="Title" className='text-custom-gradient'>EUROPEAN UNIVERSITIES</h1>
           <h2 id="Subtitle">Find Your Suitable European University</h2>
         </div>
-        <div className="flex-grow">
-        <div className="flex space-x-4 mb-8">
-        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded mb-8">Countries</button>
-        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded mb-8">Financials</button>
-        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded mb-8">Admissions</button>
-        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded mb-8">Scholarships</button>
+        <div className="flex-grow flex flex-col" id="button-container">
+          <div className="flex space-x-4 mb-8">
+            <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Countries</button>
+            <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Financials</button>
+            <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Admissions</button>
+            <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Scholarships</button>
+          </div>
         </div>
-        </div>
-        <p id="footer">This website provides information you need to be able to study in euorpe, from costs to admissions and scholarships!
-          <br></br>Studying in Europe is always a good idea! But you have to know if you are able to do it!</p>
+      </div>
+      <div className="footer-container">
+        <p id="footer">This website provides information you need to be able to study in Europe, from costs to admissions and scholarships!
+          <br />Studying in Europe is always a good idea! But you have to know if you are able to do it!</p>
       </div>
     </main>
   );
