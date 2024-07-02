13e199663d8035a5fd7764b3cb6169f3b41966f2
VoidyCd <voidycodes@gmail.com>
2024-07-02 15:10:41 +0300

    added side bar to be used later

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -8,6 +8,15 @@ const Countries = () => {
       <div className="flex-grow flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-900">
         <h1 id="Title">COUNTRIES</h1>
         <h2 id="Subtitle">Find your European Country</h2>
+
+        <div id="country-container" className="flex space-x-4 mb-8">
+        <button id="GermanButton" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Germany</button>
+        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Finland</button>
+        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Sweden</button>
+        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">United Kingdom</button>
+        <button id="Button" className="bg-custom-gradient text-white font-bold py-2 px-4 rounded">Norway</button>
+
+        </div>
       </div>
     </main>
   );
