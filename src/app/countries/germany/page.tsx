17ae7f84d572f919209d9dd4552d297a8271d62d
VoidyCd <voidycodes@gmail.com>
2024-07-09 18:27:45 +0300

    updated germany page

--- src/app/countries/germany/page.tsx
+++ src/app/countries/germany/page.tsx
@@ -3,7 +3,7 @@ import './germany.css'
 
 
 
-const Finland = () => {
+const Germany = () => {
     const CountryInformation = {
         countryName: "GERMANY",
         countryNameOptional: "GERMAN",
@@ -11,19 +11,24 @@ const Finland = () => {
         internationalAdmissionInformation: "International Admission Info Here.",
         universityInformation: "University Info Here.",
         internationalUniversityInformation: "International University Info Here.",
+        visaRequirementsRequirements: "General Visa Requirements Here.",
+        visaRequirementsFinancials: "Details about Financial Requirements Here.",
+        visaRequirementsInsurance: "Health and Travel Insurance Information Here.",
         footerInformation: "I always recommend verifying this information on official Goverment or University Websites, Don't rely on this information.",
         titleColor1: "#fe0101",
         titleColor2: "#ffe600"
     }
     return (
         <div>
-        <Country countryName={CountryInformation.countryName} countryNameOptional={CountryInformation.countryNameOptional}
-        admissionInformation={CountryInformation.admissionInformation} internationalAdmissionInformation={CountryInformation.internationalAdmissionInformation}
-         universityInformation={CountryInformation.universityInformation} internationalUniversityInformation={CountryInformation.internationalUniversityInformation}
-        footerInformation={CountryInformation.footerInformation} titleColor1={CountryInformation.titleColor1} titleColor2={CountryInformation.titleColor2} AICE={""}
-        ></Country>
-    </div>
+            <Country countryName={CountryInformation.countryName} countryNameOptional={CountryInformation.countryNameOptional}
+            admissionInformation={CountryInformation.admissionInformation} internationalAdmissionInformation={CountryInformation.internationalAdmissionInformation}
+            universityInformation={CountryInformation.universityInformation} internationalUniversityInformation={CountryInformation.internationalUniversityInformation}
+            footerInformation={CountryInformation.footerInformation} titleColor1={CountryInformation.titleColor1} titleColor2={CountryInformation.titleColor2}
+            visaRequirementsRequirements={CountryInformation.visaRequirementsRequirements} visaRequirementsFinancials={CountryInformation.visaRequirementsFinancials}
+            visaRequirementsInsurance={CountryInformation.visaRequirementsInsurance}
+            ></Country>
+        </div>
     )
 }
 
-export default Finland;
+export default Germany;
