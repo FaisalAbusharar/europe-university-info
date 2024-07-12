2d380baccbc97527aab693f1df22491031c132f4
VoidyCd <voidycodes@gmail.com>
2024-07-12 13:06:00 +0300

    added an animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client"
 import {Poppins } from 'next/font/google';
 import Link from 'next/link';
+import './animations.css'
+import { useState,useEffect } from 'react';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
 const Home = () => {
+  
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+  
   return (
-    <main className={`${poppins.className} flex flex-col min-h-screen`}>
+    <main className={`${poppins.className} flex flex-col min-h-screen ${loaded ? 'fade-in' : ''}`}>
       <div className="flex-grow flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-900 ">
         <div className="text-center mb-auto mt-8">
           <h1 id="Title" className='text-custom-gradient'>EUROPEAN UNIVERSITIES</h1>
@@ -24,7 +34,7 @@ const Home = () => {
       </div>
       <div className="footerContainer">
         <p id="footer">This website provides information you need to be able to study in Europe, from costs to admissions and scholarships!
-          <br />Studying in Europe is always a good idea! But you have to know if you are able to do it!</p>
+          <br/>Studying in Europe is always a good idea! But you have to know if you are able to do it!</p>
         <div className="credit-container">
           <p id="leftCredit">Developed by Faisal Abusharar</p>
           <p id="rightCredit">❤️Github <a id="Hyperlink" href="https://github.com/FaisalAbusharar/europe-uni-info">Open Source</a></p>
