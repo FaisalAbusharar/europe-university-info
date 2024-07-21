e5edee6fef15266d5cb7c35a8dc7290cf9469186
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2024-07-21 19:54:33 +0300

    fully designed sign up page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import '../../styles/userPage.css'
+import { Poppins, Exo } from 'next/font/google';
+
+const exo = Exo({ subsets: ['latin'], weight: ['400', '700'] });
 
 const Signup: React.FC = () => {
   const [email, setEmail] = useState<string>('');
@@ -35,39 +38,44 @@ const Signup: React.FC = () => {
   }, [submitted, router]);
 
   return (
-    <main>
-    <div className='container'>
-      {submitted ? (
-        <div className='sucessfulForm'>
-          <h1>Signup Successful!</h1>
-          <p>Redirecting to login page...</p>
-        </div>
-      ) : (
-        <div className="containerForm">
-          <h1 className='titleForm'>Signup</h1>
-          <form className='form' onSubmit={handleSignup}>
-            <input className='inputFormEmail'
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <input className='inputFormPassword'
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <button className='inputFormSumit' type="submit">Signup</button>
-          </form>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-        </div>
-      )}
-    </div>
+    <main className={`${exo.className}`}>
+      <h1 className='contextTitle'>Europe University Information</h1>
+      <p className='context'>Sign up to save important information, choose a country, learn more, and interact with others!</p>
+      <div className='container'>
+        {submitted ? (
+          <div className='sucessfulForm'>
+            <h1>Signup Successful!</h1>
+            <p>Redirecting to login page...</p>
+          </div>
+        ) : (
+          <div className="containerForm">
+            <h1 className='titleForm'>SIGN UP</h1>
+            <form className='form' onSubmit={handleSignup}>
+              <div className='inputsContainer'>
+                <input className='inputFormEmail'
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <input className='inputFormPassword'
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <button className='inputFormSubmit' type="submit">SIGN UP</button>
+            </form>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+          </div>
+        )}
+      </div>
     </main>
   );
 };
 
 export default Signup;
+
