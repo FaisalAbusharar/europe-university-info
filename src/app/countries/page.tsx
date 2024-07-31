309ca4379fcf24e3282136dcdefe60929f2996c7
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2024-07-31 23:12:06 +0300

    Revert "properly centered Countries title"
    This reverts commit 83a418304951e374210e4c008142057b9895a36b.

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -12,7 +12,7 @@ const Countries = () => {
   return (
     <main className={`${poppins.className} flex flex-col min-h-screen`}>
       <div className="flex-grow flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-900">
-        <h1 id="titleCountryPage">COUNTRIES</h1>
+        <h1 id="Title">COUNTRIES</h1>
         <h2 id="Subtitle">Find your <p id="gradientSub">European Country</p></h2>
           <div id="countryContainer" className="flex space-x-4 mb-8">
             <Link href="/countries/germany">
