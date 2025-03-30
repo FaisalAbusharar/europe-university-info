461d3c5b247dd8151887462f80f5d03dd418fca5
Voidy Devleoper <voidycodes@gmail.com>
2025-03-30 14:42:02 +0300

    Integrated Analytics fully, i hope.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/*<ThemeProvider>*/}
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children} <Analytics /></body>
       {/* </ThemeProvider> */}
     </html>
   );
