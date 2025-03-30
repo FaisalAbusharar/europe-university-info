ea8e9b8d062a10f753b4c1fb19eef8cf977f53b2
Voidy Devleoper <voidycodes@gmail.com>
2025-03-30 14:38:28 +0300

    i forgot to save this file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import {ThemeProvider} from './theme/ThemeProvider'
 import "./styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
