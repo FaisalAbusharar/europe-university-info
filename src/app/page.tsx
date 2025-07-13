03c2584b84720f4a0372335b194166f7f0905815
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2025-07-13 13:22:11 +0300

    home page is now mobile friendly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,10 +26,10 @@ const Home = () => {
   const hoverButtonColorSupport = 'linear-gradient(to right, rgb(255, 196, 3), rgb(134, 76, 0))'
 
   return (
-    <main id='mainContainer' className={`${poppins.className} flex flex-col min-h-screen ${loaded ? 'fade-in' : ''}`}>
+    <main id='mainContainer' className={`${poppins.className} flex flex-col min-h-screen overflow-x-hidden ${loaded ? 'fade-in' : ''}`}>
       <BackgroundAnim objectColor={[0, 225, 250]} className="absolute inset-0 z-0">
         <div className="flex-grow flex flex-col items-center justify-center px-4 sm:px-8">
-          <div className="text-center mb-auto mt-8">
+          <div className="text-center mb-auto mt-8 w-full max-w-[95vw] mx-auto">
             <div id="title-container">
               <Link href='/user'>
                 <button id='profile-button'>
@@ -42,26 +42,31 @@ const Home = () => {
                   />
                 </button>
               </Link>
-              <h1 id="Title" className="text-custom-gradient text-3xl sm:text-6xl break-words text-center px-2">EUROPEAN UNIVERSITIES</h1>
+
+            <h1 id="Title" 
+            className="text-custom-gradient text-4xl sm:text-5xl lg:text-[75px] text-center px-4 leading-tight translate-x-[-40px] sm:translate-x-[-50px] lg:translate-x-[-70px]"
+            >EUROPEAN UNIVERSITIES
+            </h1>
+
             </div>
-            <h2 id="Subtitle" className="text-lg sm:text-2xl">
-              Find Your <p id="gradientSubSub">Dream European University</p>
-            </h2>
+            <h2 id="Subtitle" 
+            className="text-[24px] sm:text-[32px] lg:text-[40px] text-center font-extrabold px-4 lg:mr-[70px]">
+              Find Your <p id="gradientSubSub">Dream European University</p></h2>
+
           </div>
 
           <div className="flex-grow flex flex-col w-full items-center" id="buttonContainer">
-            <div className="flex flex-wrap justify-center gap-4 mb-8 flex flex-wrap justify-center gap-4 w-full px-2">
+            <div className="flex flex-wrap justify-center gap-4 w-full px-2 mb-8">
               <Link href="/pages/countries">
-              
                 <StyledButton isDisabled={false} backgroundHoverButtonColor={hoverButtonColor}><p id="gradientButtonText">Countries</p></StyledButton>
               </Link>
               {/* <Link href="/pages/visapath"> */}
-                <StyledButton isDisabled={true} backgroundHoverButtonColor={hoverButtonColor}><p id="gradientButtonText">Permit Path</p></StyledButton>
+              <StyledButton isDisabled={true} backgroundHoverButtonColor={hoverButtonColor}><p id="gradientButtonText">Permit Path</p></StyledButton>
               {/* </Link> */}
 
               {/* <Link href="/pages/finance"> */}
               <StyledButton isDisabled={true} backgroundHoverButtonColor={hoverButtonColor}><p id="gradientButtonText">Finanicals</p></StyledButton>
-               {/* </Link> */}
+              {/* </Link> */}
 
               <Link href="/pages/admission">
                 <StyledButton isDisabled={false} backgroundHoverButtonColor={hoverButtonColor}><p id="gradientButtonText">Admissions</p></StyledButton>
@@ -78,7 +83,6 @@ const Home = () => {
               <Link href='/pages/support'>
                 <StyledButton isDisabled={false} backgroundHoverButtonColor={hoverButtonColorSupport}><p id="gradientButtonTextSUPPORT">Support</p></StyledButton>
               </Link>
-
             </div>
           </div>
         </div>
