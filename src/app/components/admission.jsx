6e4240aed666be8ceb1b9ec2dc56f11e3f69aa38
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2025-07-14 15:08:41 +0300

    another minor fix

--- src/app/components/admission.jsx
+++ src/app/components/admission.jsx
@@ -40,7 +40,7 @@ const Admission = ({
       <BackgroundAnim objectColor={backgroundColorObject} className="absolute inset-0 z-0" />
       <div className={`relative z-10 flex-grow flex-col items-center justify-center ${animation ? 'fade-enter-active' : 'fade-exit-active'}`}>
      <button id="returnButtonHeader" onClick={() => window.history.back()} aria-label="Go back"><ArrowLeft size={20} /></button>
-        <h1 className={`${poppins.className}`} id="titleCountry" style={titleStyle}>{countryName}</h1>
+        <h1 className={`text-custom-gradient text-[35px] lg:text-[75px] text-center font-extrabold grow ${poppins.className}`} id="titleCountry" style={titleStyle}>{countryName}</h1>
         <hr id="line" />
         <h2 className={`${poppins.className}`} id="subtitleLeft">{countryNameOptional} <p id="gradientSub">ADMISSION</p></h2>
         <p className={`${exo.className}`} id="infoBody">{internationalAdmissionInformation}</p>
