874b7125ef810fae7ea4048adeef7acce34c8e08
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2025-07-16 12:57:11 +0300

    support main page mobile friendly (temp solution)

--- src/app/pages/support/page.tsx
+++ src/app/pages/support/page.tsx
@@ -7,6 +7,7 @@ import Footer from '../../components/footer';
 import '../../styles/countrypage.css';
 import '../../styles/gradientButtonStyles.css';
 import '../../styles/support.css';
+import useIsMobile from '@/app/utils/useIsMobile';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 const footerInformation = 'The EUI Support page to answer all your questions.';
@@ -20,22 +21,34 @@ export default function Support() {
       .then((data) => setFaqs(data));
   }, []);
 
+  const isMobile = useIsMobile();
+
   return (
     <main className={`${poppins.className} flex flex-col min-h-screen`}>
       <div className="flex-grow flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-900">
         <button id="returnButtonHeader" onClick={() => window.history.back()} aria-label="Go back">
           <ArrowLeft size={20} />
         </button>
-        <div id="containerBox">
-          <h1 id="subtitle">Frequently Asked Questions</h1>
+        <div id={isMobile ? "containerBoxMobile" :"containerBox"}>
+          <h1 id="subtitle">{isMobile ? "Frequently Asked" : "Frequently Asked Questions"}</h1>
           <ul>
-            {faqs.map((item) => (
+            {/* This section should only show on PC, since adapting it to mobile is hard */}
+            {!isMobile && faqs.map((item) => (
               <li id="supportlist" key={item.href}>
                 <Link href={item.href} id="infoBodySS">
                   {item.title}
                 </Link>
               </li>
             ))}
+
+            {isMobile && faqs.map((item) => (
+              <li id="supportlistMobile" key={item.href}>
+                <Link href={item.href} id="infoBodySS">
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+
           </ul>
         </div>
       </div>
