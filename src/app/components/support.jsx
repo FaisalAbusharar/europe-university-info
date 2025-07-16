ccf675ad3150aea9f00cfa5e6fadae36ece9dd51
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2025-07-16 13:00:36 +0300

    support component now mobile friendly

--- src/app/components/support.jsx
+++ src/app/components/support.jsx
@@ -1,11 +1,12 @@
 "use client";
-import React from 'react';
+import React, { use } from 'react';
 import { Poppins } from 'next/font/google';
 import '../styles/countrypage.css';
 import '../styles/gradientButtonStyles.css';
 import Footer from '../components/footer';
 import '../styles/support.css';
 import { ArrowLeft } from "lucide-react";
+import useIsMobile from '../utils/useIsMobile';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -14,6 +15,9 @@ const Support = ({
   supportTitle = string, 
   supportDescription = string
 }) => {
+
+  const isMobile = useIsMobile();
+
   return (
     <main className={`${poppins.className} flex flex-col min-h-screen`}>
       <div className="flex-grow flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-900">
@@ -21,11 +25,11 @@ const Support = ({
           <ArrowLeft size={20} />
         </button>
         <div id="container">
-          <h1 id="title">{supportTitle}</h1>
+          <h1 className='text-[20px] sm:text-[20px] lg:text-[40px]' id="title">{supportTitle}</h1>
           <p id="infoBodySS">{supportDescription}</p>
         </div>
       </div>
-      <Footer footerInformation={footerInformation} />
+      <Footer footerInformation={isMobile ? "EUI Support to answer your questions" : footerInformation} />
     </main>
   );
 };
