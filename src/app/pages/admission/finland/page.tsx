7f66bfb07467e226565871146595f6c7d381112c
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2025-08-09 19:23:48 +0300

    Finland COSTS page completed.

--- src/app/pages/admission/finland/page.tsx
+++ src/app/pages/admission/finland/page.tsx
@@ -1,117 +1,115 @@
-"use client";
 
-import { Poppins, Exo } from 'next/font/google';
-import React, { useState, useEffect } from 'react';
-import BackgroundAnim from '../animation/backgroundAnimationFirst';
-import '../styles/animations.css';
-import Footer from './footer';
-import '../styles/transitions.css';
-import { ArrowLeft } from 'lucide-react';
+import Admission from '../../../components/admission';
+import '../../../styles/countrypage.css'
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
-const exo = Exo({ subsets: ['latin'], weight: ['400', '700'] });
 
-const Finance = ({
-  countryName, countryNameOptional,
-  internationalFinanceInformation,
-  footerInformation, titleColor1, titleColor2,
-  backgroundColorObject = 0, backgroundColor = '',
-  optionalInformation = [{}], internationalOptionalInformation = [{}]
-}) => {
-  const titleStyle = {    
-    background: `linear-gradient(to bottom, ${titleColor1}, ${titleColor2})`,
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent'
-  };
+const gradientStyleSpring = {
+    background: 'linear-gradient(to right, rgb(51, 204, 51), rgb(0, 204, 0), rgb(0, 153, 51))', WebkitBackgroundClip: 'text', backgroundClip: 'text', WebkitTextFillColor: 'transparent',color: 'transparent', display: 'inline-block',
+};
 
-  const [loaded, setLoaded] = useState(false);
-  const [animation, setAnimation] = useState(true);
+const gradientStyleAutumn = {
+    background: 'linear-gradient(to right, rgb(255, 133, 51), rgb(230, 92, 0))', WebkitBackgroundClip: 'text', backgroundClip: 'text', WebkitTextFillColor: 'transparent',color: 'transparent', display: 'inline-block',
+};
 
-  useEffect(() => {
-    setLoaded(true);
-  }, []);
+const gradientStyleRed = {
+    background: 'linear-gradient(to right, rgb(255, 0, 0), rgb(200, 0, 0))',WebkitBackgroundClip: 'text',backgroundClip: 'text',WebkitTextFillColor: 'transparent',color: 'transparent',display: 'inline-block',
+};
 
-  const backgroundStyle = { background: backgroundColor };
 
-  // IT tuition + living costs breakdown
-  const itCosts = [
-    {
-      title: "Tuition Fees (IT Programs)",
-      description: `Bachelor’s: €5,000–€18,000/year  
-Master’s: €9,000–€18,000/year  
-Examples: Aalto University (BSc Science & Tech) €12,000/year, University of Helsinki (Data Science) €13,000/year, Tampere University (MSc IT) €12,000/year.`
-    },
-    {
-      title: "Living Expenses",
-      description: `€700–€1,200/month depending on city.  
-Helsinki ~ €900–€1,300, Tampere ~ €700–€1,100, Oulu ~ €700–€900.  
-Covers housing, transport, food, utilities.`
-    },
-    {
-      title: "Estimated Annual Total",
-      description: `Typical IT student budget: €16,400 – €28,600/year (tuition + living).`
-    },
-    {
-      title: "Scholarship Opportunities",
-      description: `Many universities offer 50–100% tuition waivers. Finland Scholarship covers full tuition + €5,000 relocation grant.`
+
+const internationalAdmissionInformationText = 
+<>
+     
+    The admission in Finland can be quite complicated in Finland, especially for international students, we will try to break it down. <br/> Universities and Universities of applied sciences both have different admission requirements.
+    <br/> <span id="subSubtitle"> <span className='text-[24px] sm:text-[24px] lg:text-[40px] ' id="gradientSubSub">University of Applied Sciences</span></span> <br/>
+    <span id='warning'>Information here might change from year to year, this information is up-to date for 2025</span>. UAS admission for International students is relatively straight-forward, you can apply in 4 main methods,
+    The <a href='https://www.uasinfo.fi/international-uas-exam/' id='hyperlink'>International UAS Spring Exam</a> ‎  <a href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/' id='hyperlink'>International UAS Autumn Exam</a>
+    ‎ ‎ <a href='https://www.uasinfo.fi/sat-test/' id='hyperlink'>SAT Test</a> ‎ <a href='https://www.uasinfo.fi/certificate-based-selection/' id='hyperlink'>Certifcate Based Selection</a>.
+    <br></br><br></br>
+    Applications for Finnish universities typically open in January each year. For the 2025–2026 academic year, the main joint application period for English-taught Bachelor's and Master's degree programmes is scheduled from <strong><i>January 8 to January 22, 2025.</i></strong> 
+    <br/><br/><a style={gradientStyleSpring} id='gradientSubSub'><span id='subsubSubtitle'>International UAS Spring Exam</span></a><br/>
+    <strong>91 study programs</strong> use the International UAS Exam, you can view them <a href='https://www.uasinfo.fi/selection-methods-study-programmes-available-for-application/#table1spring' id='hyperlink'>here</a>
+    , Regardless of how many study programs you’re applying for, you only need to participate in the International UAS Exam <strong>once</strong>
+    . The International UAS Exam is organised on <strong>21 March 2024</strong> from 12:00 o’clock noon onwards (Finnish time, UTC/GMT+2). In the International UAS exam, the applicants’ readiness for UAS studies is assessed. The exam does not include any advance reading materials or pre-assignments.
+     In order to be invited to the International UAS Exam and to be selected based on the International UAS Exam selection method, the applicants have to <strong>prove their English language skills</strong> either by an internationally recognized language test or previous studies.
+     For more specifics, check the <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam/#languagerequirementsspring'>official site</a>. A valid passport must be present, can be biometric or non-biometric, however EU/EEA passports <strong>must be biometric. </strong>
+     The international UAS exam has different exam sections depending on your area of study, however all areas of study will include <strong>Reasoning Skill section and English Language Skills</strong> section.
+     <br/> <span id='warning'>Some important details are not listed here, please check the official site for ALL relevant important information.</span>
+     <br/> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam/#identificationspring'>Identification</a> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam/#monitoringspring'>Monitoring</a>‎
+     ‎<a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam/#privacypolicyspring'>Privacy Policy</a> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam/#hardwareandnetwork'>Hardware/Network Requirement</a>‎
+     ‎<a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam/#participatingspring'>Participating</a> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam/#results'>Final Student Selection</a>  
+
+     <br/><br/><a style={gradientStyleAutumn} id='gradientSubSub'><span id='subsubSubtitle'>International UAS Autumn Exam</span></a><br/>
+    <strong>In the autumn joint application, 8 study programs</strong> use the International UAS Exam, you can view them <a href='https://www.uasinfo.fi/selection-methods-study-programmes-available-for-application/#table1autumn' id='hyperlink'>here</a>
+    , Regardless of how many study programs you’re applying for, you only need to participate in the International UAS Exam <strong>once</strong>
+    . The International UAS Exam is organised on <strong>7 Novemeber 2024</strong> from 12:00 o’clock noon onwards (Finnish time, UTC/GMT+2). In the International UAS exam, the applicants’ readiness for UAS studies is assessed. The exam does not include any advance reading materials or pre-assignments.
+     In order to be invited to the International UAS Exam and to be selected based on the International UAS Exam selection method, the applicants have to <strong>prove their English language skills</strong> either by an internationally recognized language test or previous studies.
+     For more specifics, check the <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/#languagerequirements'>official site</a>. A valid passport must be present, can be biometric or non-biometric, however EU/EEA passports <strong>must be biometric. </strong>
+     The international UAS exam has different exam sections depending on your area of study, however all areas of study will include <strong>Reasoning Skill section and English Language Skills</strong> section.
+     <br/> <span id='warning'>Some important details are not listed here, please check the official site for ALL relevant important information.</span>
+     <br/> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/#identificationautumn'>Identification</a> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/#monitoringautumn'>Monitoring</a>‎
+     ‎<a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/#privacypolicy'>Privacy Policy</a> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/#hardwarerequirements'>Hardware/Network Requirement</a>‎
+     ‎<a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/#participatingautumn'>Participating</a> <a id='hyperlink' href='https://www.uasinfo.fi/international-uas-exam-autumn-2024/#resultsautumn'>Final Student Selection</a>  
+     <br/><br/><a style={gradientStyleRed} id='gradientSubSub'><span id='subsubSubtitle'>SAT Test</span></a><br/>
+     The SAT test contains following sections:
+
+    <br/><strong>Evidence-Based Reading:</strong> 
+    <strong> Writing
+    Reading Test, </strong>
+     <strong> Writing</strong> and <strong>Language Test </strong>
+    | <strong>Math</strong>
+    <br></br>
+    In their application instructions, universities of applied sciences announce the SAT sections they will use as an admission criterion in the study programme available for application, and the lowest score <strong>limit the applicant must reach in order to be qualified</strong> for the selection process. Only the new version of SAT test (completed after March 2016) is accepted. The SAT test selection only takes into account the sections performed during a single session.
+    <strong> An official SAT score report must be ordered directly from the test organiser</strong>. To collect SAT test scores, universities of applied sciences can also use an electronic service from which the score data is available. The applicants who have already completed an SAT test must request a test result from the test organiser to be delivered to universities of applied sciences. Applicants must submit the SAT test results to all universities of applied sciences that use said selection procedure and to which they are applying within the provided timetable. <strong>Reports printed or submitted by the applicants themselves cannot be taken into account</strong>.
+
+    <br/><br/><a id='gradientSubSub'><span id='subsubSubtitle'>Certificate-based Selection</span></a><br/>
+    Universities of applied sciences may admit students by means of certificate-based selection based on matriculation examination and vocational upper secondary qualification (completed after 1 August 2015). Those applying with a matriculation examination (Finnish or EB, IB and RP/DIA diploma) or vocational upper secondary qualification have a specific study place quota which varies between study programmes available for application.
+    All universities of applied sciences comply with the scoring models and principles of certificate-based selection. In certificate-based selection, the university of applied sciences may independently determine the lowest score limit or the threshold criteria.<br></br>
+    Get all detailed information <a href='https://www.uasinfo.fi/certificate-based-selection/' id='hyperlink' >here</a>.
+
+
+</>
+
+
+const backgroundColorStyle = 'linear-gradient(to bottom, rgb(0, 0, 0), rgb(0, 26, 51))'
+const backgroundColorObjectStyle: number[] = [0, 50, 150]
+
+const optionalInformationImportantInfo = <>
+  Applicants who apply before obtaining their final diploma, such as A-level students applying with predicted grades, must <strong>provide the final diploma</strong> by <strong>July 10th</strong> (<em>For Spring Joint Application</em>). However, some students, like A-level students, may not receive their final diploma until August 13th. For such cases, Finnish universities offer a solution: you must provide a <strong>provisional certificate of graduation</strong> issued and stamped by your school, confirming that you have completed the required materials.
+  You will most likely not find information about this online, this information was from the <strong>admission services</strong> for multiple UAS universities (<em>Tampere UAS, Oulu UAS, Jamk UAS</em>), you can contact them directly for further questions.
+</>;
+
+
+
+const Finland = () => {
+    const CountryInformation = {
+        countryName: "FINLAND",
+        countryNameOptional: "FINNISH",
+        internationalAdmissionInformation: internationalAdmissionInformationText,
+        footerInformation: "We always recommend verifying this information on official Goverment or University Websites, Don't rely on this information.",
+        titleColor1: "#003580",
+        titleColor2: "#ffffff",
+        backgroundColor: backgroundColorStyle,
+        backgroundColorObject: backgroundColorObjectStyle,
+        optionalInformation: [
+            {title: <><span id="gradientSub">Deadline Information</span></>, description: optionalInformationImportantInfo},
+            //{title:  <><span id="gradientSub"></span> OR <span id="gradientSubSub">U</span></>, description: },
+        ],
+        internationalOptionalInformation: [
+            //{title: <><span id="gradientSubSub"></span></>, description: },
+        ],
+
     }
-  ];
-
-  return (
-    <main style={backgroundStyle} id="backgroundCountry" className={`${exo.className} flex flex-col min-h-screen ${loaded ? 'slide-in-top' : ''}`}>
-      <BackgroundAnim objectColor={backgroundColorObject} className="absolute inset-0 z-0" />
-      <div className={`relative z-10 flex-grow flex-col items-center justify-center ${animation ? 'fade-enter-active' : 'fade-exit-active'}`}>
-        <button id="returnButtonHeader" onClick={() => window.history.back()} aria-label="Go back">
-          <ArrowLeft size={20} />
-        </button>
-
-        <h1 className={`text-custom-gradient text-[35px] lg:text-[75px] text-center font-extrabold grow ${poppins.className}`} id="titleCountry" style={titleStyle}>
-          {countryName}
-        </h1>
-        <hr id="line" />
-
-        <h2 className={`text-[30px] sm:text-[24px] lg:text-[40px] ${poppins.className}`} id="subtitleLeft">
-          {countryNameOptional} <p id="gradientSub">ADMISSION</p>
-        </h2>
-
-        <p className={`${exo.className}`} id="infoBody">{internationalFinanceInformation}</p>
-
-        {/* Original optional information */}
-        <ul className={`${exo.className}`} id="infoBody">
-          {(optionalInformation || []).map((item, index) => (
-            <li key={index}>
-              <p className={`${exo.className}`} id="subSubTitleLeft"><strong>{item.title}</strong></p>
-              <small className={`${exo.className}`} id="infoBody">{item.description}</small>
-            </li>
-          ))}
-        </ul>
-
-        {/* Original international optional information */}
-        <ul className={`${exo.className}`} id="infoBody">
-          {(internationalOptionalInformation || []).map((item, index) => (
-            <li key={index}>
-              <p className={`${exo.className}`} id="subSubTitleLeft"><strong>{item.title}</strong></p>
-              <small className={`${exo.className}`} id="infoBody">{item.description}</small>
-            </li>
-          ))}
-        </ul>
-
-        {/* New IT tuition + cost breakdown */}
-        <h2 className={`${poppins.className} text-[28px] mt-6`} id="gradientSub">IT Tuition & Cost Details</h2>
-        <ul className={`${exo.className}`} id="infoBody">
-          {itCosts.map((item, index) => (
-            <li key={index}>
-              <p className={`${exo.className}`} id="subSubTitleLeft"><strong>{item.title}</strong></p>
-              <small className={`${exo.className}`} id="infoBody">{item.description}</small>
-            </li>
-          ))}
-        </ul>
-
-      </div>
-      <div id="buffer"></div>
-      <Footer showAIwarning={true} footerInformation={footerInformation}></Footer>
-    </main>
-  );
+    return (
+        <div>
+            <Admission countryName={CountryInformation.countryName} countryNameOptional={CountryInformation.countryNameOptional}
+            internationalAdmissionInformation={CountryInformation.internationalAdmissionInformation}
+            footerInformation={CountryInformation.footerInformation} titleColor1={CountryInformation.titleColor1} titleColor2={CountryInformation.titleColor2}
+            optionalInformation={CountryInformation.optionalInformation} internationalOptionalInformation={CountryInformation.internationalOptionalInformation}
+            backgroundColor={CountryInformation.backgroundColor} backgroundColorObject={CountryInformation.backgroundColorObject}
+            ></Admission>
+        </div>
+    )
 }
 
-export default Finance;
+export default Finland;
--- src/app/pages/finance/finland/page.tsx
+++ src/app/pages/finance/finland/page.tsx
@@ -1,66 +1,90 @@
 import Finance from '../../../components/finance';
 import '../../../styles/countrypage.css'
 
+
+const gradientStyleGreen = {
+    background: 'linear-gradient(to right, rgb(51, 204, 51), rgb(0, 204, 0), rgb(0, 153, 51))', WebkitBackgroundClip: 'text', backgroundClip: 'text', WebkitTextFillColor: 'transparent',color: 'transparent', display: 'inline-block',
+};
+
+const gradientStyleBrownOrange = {
+    background: 'linear-gradient(to right, rgb(255, 133, 51), rgb(230, 92, 0))', WebkitBackgroundClip: 'text', backgroundClip: 'text', WebkitTextFillColor: 'transparent',color: 'transparent', display: 'inline-block',
+};
+
+const gradientStyleRed = {
+    background: 'linear-gradient(to right, rgb(255, 0, 0), rgb(200, 0, 0))',WebkitBackgroundClip: 'text',backgroundClip: 'text',WebkitTextFillColor: 'transparent',color: 'transparent',display: 'inline-block',
+};
+
+const gradientStyleBlue = {
+    background: 'linear-gradient(to right, rgb(0, 13, 255), rgb(0, 87, 200))',WebkitBackgroundClip: 'text',backgroundClip: 'text',WebkitTextFillColor: 'transparent',color: 'transparent',display: 'inline-block',
+};
+
+const gradientStyleYellow = {
+    background: 'linear-gradient(to right, rgb(255, 247, 0), rgb(200, 123, 0))',WebkitBackgroundClip: 'text',backgroundClip: 'text',WebkitTextFillColor: 'transparent',color: 'transparent',display: 'inline-block',
+};
+
+
 const internationalFinanceInformationText = 
 <>
-  <p><strong>University Tuition Fees in Finland (for Non-EU/EEA Students)</strong></p>
+  <a style={gradientStyleGreen} id="gradientSubSub" className="text-2xl font-bold mt-6 mb-2">
+    University Tuition Fees in Finland (Non-EU/EEA Students)
+  </a>
   <p>
-    Tuition fees in Finland vary by university, degree level, and field of study.  
-    The figures below are typical for English-taught Bachelor’s and Master’s programs.
+    Tuition fees for international students in Finland vary widely depending on the university, the degree level, and the chosen field of study. 
+    For English-taught <strong>Bachelor’s programs</strong>, fees typically range between €5,000 and €18,000 per academic year, while 
+    <strong> Master’s programs</strong> are usually priced between €9,000 and €18,000. Some highly specialized degrees, such as Medicine, 
+    can exceed these ranges significantly.
   </p>
-
-  <ul>
-    <li>
-      <strong>Information Technology / Engineering / Computer Science:</strong><br/>
-      Bachelor’s: €5,000 – €18,000/year  
-      Master’s: €9,000 – €18,000/year  
-      Examples: Aalto University MSc Computer Science ~ €15,000/year, University of Oulu BSc IT ~ €10,000/year.
-    </li>
-
-    <li>
-      <strong>Business, Economics, Management:</strong><br/>
-      Bachelor’s: €6,000 – €15,000/year  
-      Master’s: €10,000 – €18,000/year  
-      Example: Hanken School of Economics MSc Economics ~ €12,500/year.
-    </li>
-
-    <li>
-      <strong>Arts, Design, Architecture:</strong><br/>
-      Bachelor’s: €6,000 – €15,000/year  
-      Master’s: €10,000 – €18,000/year  
-      Example: Aalto University MA in Design ~ €15,000/year.
-    </li>
-
-    <li>
-      <strong>Natural Sciences & Mathematics:</strong><br/>
-      Bachelor’s: €5,000 – €13,000/year  
-      Master’s: €9,000 – €16,000/year  
-      Example: University of Helsinki MSc Data Science ~ €13,000/year.
-    </li>
-
-    <li>
-      <strong>Medicine & Health Sciences:</strong><br/>
-      Medicine: €12,000 – €25,000/year (very limited spots for non-EU students)  
-      Nursing & Allied Health: €6,000 – €12,000/year.
-    </li>
-  </ul>
-
-  <p><strong>Living Expenses:</strong>  
-    €700 – €1,200/month depending on city.  
-    Covers housing (€350–€700), food (€200–€350), transport (€50–€70), and personal expenses.
+  <br></br>
+  <h3 style={gradientStyleRed} id="gradientSubSub" className="text-xl font-semibold mt-4 mb-1">Field-Specific Trends</h3>
+  <p>
+    In <strong>Information Technology, Engineering, and Computer Science</strong>, Bachelor’s programs often cost between €5,000 and €18,000 annually, 
+    while Master’s degrees average €12,000–€16,000 per year. For example, the University of Oulu charges around €10,000/year for a Bachelor’s in IT, 
+    and Aalto University’s Master’s in Computer Science is approximately €15,000/year.
   </p>
-
-  <p><strong>Annual Budget Estimate (Tuition + Living):</strong><br/>
-    Bachelor’s: €14,000 – €32,000/year  
-    Master’s: €18,000 – €34,000/year  
-    Medicine: €25,000 – €40,000/year
+  <br></br>
+  <p>
+    <strong>Business, Economics, and Management</strong> degrees tend to be slightly more expensive, with Bachelor’s programs ranging from €6,000 to €15,000, 
+    and Master’s programs generally between €10,000 and €18,000. A well-known example is Hanken School of Economics, which charges about €12,500/year for its Master’s in Economics.
   </p>
+  <br></br>
+  <p>
+    In <strong>Arts, Design, and Architecture</strong>, Bachelor’s and Master’s fees usually fall between €6,000 and €15,000, with prestigious programs, 
+    such as Aalto University’s Master’s in Design, reaching €15,000/year.
+  </p>
+  <br></br>
+  <p>
+    For <strong>Natural Sciences and Mathematics</strong>, Bachelor’s tuition generally ranges from €5,000 to €13,000, and Master’s from €9,000 to €16,000. 
+    A typical example is the University of Helsinki’s MSc in Data Science, which is priced at around €13,000/year.
+  </p>
+  <br></br>
+  <p>
+    <strong>Medicine and Health Sciences</strong> stand apart in terms of cost and selectivity. Medicine can cost anywhere from €12,000 to €25,000/year, 
+    with very few spots available for non-EU students. Nursing and other allied health degrees are typically more affordable, usually between €6,000 and €12,000 annually.
+  </p>
+  <br></br>
 
-  <p><strong>Scholarships:</strong>  
-    - Finland Scholarship: full tuition + €5,000 relocation grant (Master’s level).<br/>
-    - University-specific merit scholarships (25%–100% tuition waiver).
+
+  <h3 style={gradientStyleBlue} id="gradientSubSub" className="text-xl font-semibold mt-4 mb-1">Living Costs in Finland</h3>
+  <p>
+    In addition to tuition, students need to budget for living expenses, which typically range from €700 to €1,200 per month depending on the city. 
+    This covers housing (approximately €350–€700), food (€200–€350), transportation (€50–€70), and personal expenses.
+  </p>
+  <br></br>
+  <h3 style={gradientStyleBrownOrange} id="gradientSubSub" className="text-xl font-semibold mt-4 mb-1">Scholarship Opportunities</h3>
+  <p>
+    Many universities offer merit-based scholarships covering 25% to 100% of tuition fees. At the Master’s level, the <strong>Finland Scholarship</strong>  is available, which includes full tuition coverage and a €5,000 relocation grant. Each institution has its own criteria, so applicants should check 
+    directly with their chosen university.
+  </p>
+  <br></br>
+  <h3 id="gradientSubSub" style={gradientStyleYellow} className="text-xl font-semibold mt-4 mb-1">Average Annual Cost of Study</h3>
+  <p>
+    Taking averages across fields, a typical Bachelor’s program for a non-EU student in Finland costs about €10,000/year in tuition, while Master’s programs 
+    average €13,000/year. Medicine is considerably higher, averaging around €18,000/year. When adding average living expenses of roughly €9,000 annually, 
+    the total estimated yearly cost is about <strong>€19,000 for Bachelor’s</strong>, <strong>€22,000 for Master’s</strong>, and <strong>€27,000 for Medicine</strong>. 
+    These figures can vary depending on lifestyle choices, university location, and available scholarships.
   </p>
 </>
+
 
 const backgroundColorStyle = 'linear-gradient(to bottom, rgb(0, 0, 0), rgb(0, 26, 51))'
 const backgroundColorObjectStyle: number[] = [0, 50, 150]
